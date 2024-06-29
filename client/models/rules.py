bf5e958174bd46bc5d6c55ecf0aff6aa24a78d19
rb5883 <rb58853@gmail.com>
2024-06-29 18:25:22 -0400

    save

--- client/models/rules.py
+++ client/models/rules.py
@@ -1,9 +1,12 @@
 import functools
+import inspect
 
 
 class ClientRules:
-    def __init__(self, len_spaces_range=(3, 5)):
+    def __init__(self, len_spaces_range=(3, 5), valid_characters=r"^[a-zA-Z0-9 ]+$"):
+        self.not_rules = ["__call__", "__init__"]
         self.len_spaces_range = len_spaces_range
+        self.valid_characters = valid_characters
 
     def __call__(self, func):
         def wrapper(*args, **kwargs):
@@ -11,9 +14,10 @@ class ClientRules:
             chain = args[1]
             result_chain = ("ok", chain)
 
-            spaces_rules = self.spaces_rule(chain)
-            if spaces_rules:
-                return self.func(spaces_rules)
+            rules = inspect.getmembers(ClientRules, predicate=inspect.isfunction)
+            for rule_name, rule in rules:
+                if rule_name not in self.not_rules:
+                    rule(self, chain)
 
             return func(self=chains_self, chain=result_chain)
 
@@ -28,23 +32,16 @@ class ClientRules:
         max_spaces = self.len_spaces_range[1]
 
         if count_spaces > max_spaces or count_spaces < min_spaces:
-            return (
-                "error",
-                f"the chains should have between {min_spaces} and {max_spaces} spaces",
+            return SyntaxError(
+                f"the chains should have between {min_spaces} and {max_spaces} spaces"
             )
 
         for i in range(len(chain) - 1):
             if chain[i] == " " and chain[i + 1] == " ":
-                return ("error", "the chains can not contain consecutive spaces")
+                return SyntaxError("the chains can not contain consecutive spaces")
 
         return False
 
     def caracters_rule(self, chain):
-        if chain[:1] == " " or chain[-1:] == " ":
-            return ("error", "the chains can not contain spaces in init or end")
-
-        count_spaces = chain.count(" ")
-        if count_spaces > 5 or count_spaces < 3:
-            return ("error", "the chains should have between 3 and 5 spaces")
 
         return False
